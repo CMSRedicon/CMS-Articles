c62faedc587e8a876fb3f2385372dd6bc861eae2
Tomasz Cwik <t.cwika@clickonometrics.pl>
2018-10-01 11:15:09 +0200

    gulp

---
+++ src/public/assets/src/ts/frontend.ts
@@ -0,0 +1,20 @@
+class Frontend {
+
+    /**
+     * Konstruktor
+     */
+    constructor() {
+        this.start;
+    }
+
+
+    /**
+     * Starter
+     * @returns void
+     */
+    private start = (): void => {
+        alert('działa!1');
+    }
+
+}
+
