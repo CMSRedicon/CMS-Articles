3511254ac464d775f6757c3f5fc340715338e8da
Tomasz Cwik <t.cwika@clickonometrics.pl>
2018-10-01 14:14:21 +0200

    fix

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,5 +1,4 @@
 <?php
-
-Route::group(['middleware' => ['auth', 'language']], function () {
-    Route::get('admin/articles', 'Redicon\CMS_Articles\App\Http\Controllers\Admin\ArticlesController@index');
+Route::group(['middleware' => ['web','auth', 'language']], function () {
+    Route::get('/admin/articles', 'Redicon\CMS_Articles\App\Http\Controllers\Admin\ArticlesController@index');
 });
