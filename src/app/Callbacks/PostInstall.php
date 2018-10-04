5380aa8cbbc7806898c7bf0ac8d67d50a5e76c2c
Tomasz Cwik <t.cwika@clickonometrics.pl>
2018-10-04 16:10:16 +0200

    cms

--- src/app/Callbacks/PostInstall.php
+++ src/app/Callbacks/PostInstall.php
@@ -3,23 +3,12 @@ namespace App\Callbacks;
 
 class PostInstall {
 
-    private $parent_vendor_path;
-
-    public function __construct(String $parent_vendor_path)
-    {
-        $this->parent_vendor_path = $parent_vendor_path;
-    }
-
-    public function run(){
-
-        require_once $this->parent_vendor_path;
-
-        try{
-            \Illuminate\Support\Facades\Artisan::call('migrate:fresh', ['--path' => 'vendor\redicon\cms_articles\src\database\migrations']);
-            \Illuminate\Support\Facades\Artisan::call('db:seed', ['--class' => 'Redicon\CMS_Articles\Database\Seeds\DatabaseSeeder']);
-        }catch(\Exception $e){
-            echo $e->getMessage() . PHP_EOL;
-        }
+    public function getCommands() : array{
+    
+        return [
+        'php artisan migrate:fresh --path=vendor\redicon\cms_articles\src\database\migrations',
+        'php artisan db:seed --class=Redicon\CMS_Articles\Database\Seeds\DatabaseSeeder'
+        ];
         
     }
 
