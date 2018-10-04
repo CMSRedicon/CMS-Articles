1699a273c1b2efc46bc50937de5bf257b3f734e0
Tomasz Cwik <t.cwika@clickonometrics.pl>
2018-10-04 15:09:28 +0200

    cms

--- src/app/Callbacks/PostInstall.php
+++ src/app/Callbacks/PostInstall.php
@@ -1,11 +1,11 @@
 <?php
-use Artisan;
+use Illuminate\Support\Facades\Artisan;
 namespace App\Callbacks;
 
 class PostInstall {
 
     public function run(){
-        
+
         try{
             Artisan::call('migrate:fresh', ['--path' => 'vendor\redicon\cms_articles\src\database\migrations']);
             Artisan::call('db:seed', ['--class' => 'Redicon\CMS_Articles\Database\Seeds\DatabaseSeeder']);
