7279d3546b4078d5bed2e6a77e2f3c7c5f560c8b
Tomasz Cwik <t.cwika@clickonometrics.pl>
2018-10-04 15:20:12 +0200

    cms

--- src/app/Callbacks/PostInstall.php
+++ src/app/Callbacks/PostInstall.php
@@ -3,7 +3,16 @@ namespace App\Callbacks;
 
 class PostInstall {
 
+    private $parent_vendor_path;
+
+    public function __construct(String $parent_vendor_path)
+    {
+        $this->parent_vendor_path = $parent_vendor_path;
+    }
+
     public function run(){
+
+        require_once $this->parent_vendor_path;
 
         try{
             \Illuminate\Support\Facades\Artisan::call('migrate:fresh', ['--path' => 'vendor\redicon\cms_articles\src\database\migrations']);
